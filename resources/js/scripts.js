0402582d9af276406d838ceb6ae7f12c0c450af3
Oleksii Mostovyi <mostovoy.alexey@gmail.com>
2018-08-30 14:15:13 +0300

    Optimization

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // For the Sticky Navigation
     $('.js--section-features').waypoint(function (direction) {
@@ -13,25 +13,28 @@ $(document).ready(function() {
 
     // Scroll on Buttons
     $('.js--scroll-to-plans').click(function () {
-        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
     });
 
     $('.js--scroll-to-start').click(function () {
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
     });
 
     // Navigation scroll
-// Select all links with hashes
+    // Select all links with hashes
     $('a[href*="#"]')
-    // Remove links that don't actually link to anything
+        // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
-        .click(function(event) {
+        .click(function (event) {
             // On-page links
             if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname === this.hostname
             ) {
                 // Figure out element to scroll to
                 var target = $(this.hash);
@@ -42,7 +45,7 @@ $(document).ready(function() {
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 1000, function() {
+                    }, 1000, function () {
                         // Callback after animation
                         // Must change focus!
                         var $target = $(target);
@@ -50,7 +53,7 @@ $(document).ready(function() {
                         if ($target.is(":focus")) { // Checking if the target was focused
                             return false;
                         } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
                             $target.focus(); // Set focus again
                         };
                     });
@@ -87,7 +90,7 @@ $(document).ready(function() {
     $('.js--nav-icon').click(function () {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         nav.slideToggle(200);
         if (icon.hasClass('ion-navicon-round')) {
             icon.addClass('ion-close-round');
@@ -98,4 +101,3 @@ $(document).ready(function() {
         }
     })
 });
-
